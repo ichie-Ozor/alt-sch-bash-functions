dc5144addc04590f8b2669c09f76188595ce449e
Ichie Ozor <simeon_mc2000@yahoo.com>
2025-01-10 13:17:47 +0100

    my work

---
+++ ozoemena.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+who_am_i(){
+    echo "Hello, My name is Ozoemena"
+}
+
+Fullname(){
+    echo "Ozoemena Simeon Uroko"
+}
+
+Bio(){
+    echo "I am a full stack web developer with interest in mobile applications"
+    echo "I work with Brainstorm IT solutions"
+    echo "I live in Kano, Nigeria"
+}
+
+Years_in_tech(){
+    echo "I have over 3 years experience in tech"
+}
+
+Dream_job(){
+    echo "I will like to work in a nigerian bank as a software developer"
+    echo "I will want to work with google in the future"
+}
+
+Goals(){
+    echo "I want to learn C# by the end of march"
+    echo "I want to learn how to configure an empty server by march 2025"
+    echo "I want to get married by June 2025 to the love of my life"
+    echo "I want to have 100 people using my app by june 2025"
+}
+
+who_am_i
+Fullname
+Bio
+Years_in_tech
+Dream_job
+Goals
